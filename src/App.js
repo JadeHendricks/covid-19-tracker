c46bc2c0ccb7f5cedc71ade36aeb41b887525a42
jadehendricks <hendricksjade@gmail.com>
2020-09-08 18:11:55 +0200

    project update

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import {
   Card,
   CardContent,
 } from "@material-ui/core";
-import InfoBox from "./components/InfoBox";
-import LineGraph from "./components/LineGraph";
-import Table from "./components/Table";
-import { sortData, prettyPrintStat } from "./utils";
+import InfoBox from "./InfoBox";
+import LineGraph from "./LineGraph";
+import Table from "./Table";
+import { sortData, prettyPrintStat } from "./util";
 import numeral from "numeral";
-import Map from "./components/Map";
+import Map from "./Map";
 import "leaflet/dist/leaflet.css";
 
 const App = () => {
